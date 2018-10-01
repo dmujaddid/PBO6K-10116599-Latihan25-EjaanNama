e6722c8cf8d1731014de4d544e49f108b4297409
Roby <robyeka@email.unikom.ac.id>
2018-10-02 02:28:33 +0700

    first commit

--- src/pbo6k/pkg10116599/latihan25/ejaannama/PBO6K10116599Latihan25EjaanNama.java
+++ src/pbo6k/pkg10116599/latihan25/ejaannama/PBO6K10116599Latihan25EjaanNama.java
@@ -5,13 +5,15 @@
  */
 package pbo6k.pkg10116599.latihan25.ejaannama;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 
  * NAMA     : Robby Eka Purnama
  * KELAS    : PBO6K
  * NIM      : 10116599
- * Deskripsi Program : Program ejaan nama yang inputannya berasal dari user.
+ * Deskripsi Program : Program menampilkan ejaan nama yang inputannya berasal dari user.
  * 
  */
 public class PBO6K10116599Latihan25EjaanNama {
@@ -21,6 +23,24 @@ public class PBO6K10116599Latihan25EjaanNama {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.print("Masukkan nama depan Anda untuk dieja : ");
+        Scanner scan = new Scanner(System.in);
+        String nama = scan.next();
+        System.out.println();
+        System.out.println("Ejaan untuk '" +nama+ "' adalah :");
+       
+       
+        char [] charArray = nama.toCharArray();
+        int sz = charArray.length;
+        
+       for(int x=1; x <= sz; x++) 
+            System.out.println("Huruf ke-" +x+ " : ");
+        
+       for (char c : charArray) {
+            System.out.println(c);
+ 
+        }
     }
     
 }
